e7a16b64fa31711be89eb31da16a518f85400d22
Huib Keemink <huib.keemink@creativedutchmen.com>
2019-09-25 13:10:45 +0200

    Fixed setup.py to include nested files

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -6,6 +6,6 @@ with open('requirements.txt', 'r') as f:
 setup(
     name="{{ cookiecutter.project_slug }}",
     version="0.0.0",
-    packages=find_packages(include=['{{ cookiecutter.project_slug }}',]),
+    packages=find_packages(include=['{{ cookiecutter.project_slug }}', '{{ cookiecutter.project_slug }}/*']),
     install_requires=install_requires,
 )
